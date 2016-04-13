6721d060b1f933d85a589ecf6175fe9d2258b6db
jgal <jgal@purdue.edu>
2016-04-13 10:43:23 -0400

    Webhook added

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ app.get('/', function(request, response) {
 app.get('/cool', function(request, response) {
 	response.send(cool());
 });
+
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === <YOUR_VERIFY_TOKEN>) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');    
+  }
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
