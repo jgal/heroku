9bc47415dcd6bc30a67432ad8d2c242bf53b5338
jgal <jgal@purdue.edu>
2016-04-13 16:02:31 -0400

    listen for messages

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ app.get('/webhook', function (request, response) {
   } else {
     response.send('Error, wrong validation token');    
   }
+
+  messaging_events = request.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = request.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+    }
+  }
+  response.sendStatus(200);
 });
 
 app.listen(app.get('port'), function() {
