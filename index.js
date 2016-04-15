2bba7cc4690a6e992411a4553dd56bb2bd68e7d9
jgal <jgal@purdue.edu>
2016-04-14 23:15:18 -0400

    text received

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/webhook/', function (req, res) {
             if (text === "What's your name?") {
               sendTextMessage(sender, "Jaankabot")
             } else 
-              sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+              sendTextMessage(sender, "Text received: " + text.substring(0, 200))
         }
     }
     res.sendStatus(200)
